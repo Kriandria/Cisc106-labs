7e669fc9c050798adffcf8d331ece274a2053cda
Dylan Leh <ruinsane@fedoraproject.org>
2015-10-06 12:31:35 -0400

    Create project1_shellscript.py

---
+++ Project-1/project1_shellscript.py
@@ -0,0 +1,285 @@
+Python 3.4.3 (v3.4.3:9b73f1c3e601, Feb 23 2015, 02:52:03) 
+[GCC 4.2.1 (Apple Inc. build 5666) (dot 3)] on darwin
+Type "copyright", "credits" or "license()" for more information.
+>>> ================================ RESTART ================================
+>>> 
+MENU
+(a) Addition
+(s) Subtraction
+(m) Multiplication
+(d) Division
+(e) Exponentiation
+(r) Random Type
+(q) Quit
+Enter Your Choice:thisIsAnInvalidChoice
+Error: Invalid selection, please enter a valid choice.’
+MENU
+(a) Addition
+(s) Subtraction
+(m) Multiplication
+(d) Division
+(e) Exponentiation
+(r) Random Type
+(q) Quit
+Enter Your Choice:howAboutThis?
+Error: Invalid selection, please enter a valid choice.’
+MENU
+(a) Addition
+(s) Subtraction
+(m) Multiplication
+(d) Division
+(e) Exponentiation
+(r) Random Type
+(q) Quit
+Enter Your Choice:Okay,youwin
+Error: Invalid selection, please enter a valid choice.’
+MENU
+(a) Addition
+(s) Subtraction
+(m) Multiplication
+(d) Division
+(e) Exponentiation
+(r) Random Type
+(q) Quit
+Enter Your Choice:a
+Addition
+
+What is 20 + 10?: 30
+That is correct!
+
+What is 14 + 11?: 25
+That is correct!
+
+What is 6 + 4?: 10
+That is correct!
+
+What is 14 + 11?: 1
+That is the incorrect answer. Please Try again.
+What is 14 + 11?: 2
+That is the incorrect answer. Please Try again.
+What is 14 + 11?: 6
+That is incorrect, the correct answer was 25
+
+MENU
+(a) Addition
+(s) Subtraction
+(m) Multiplication
+(d) Division
+(e) Exponentiation
+(r) Random Type
+(q) Quit
+Enter Your Choice:s
+Subtraction
+
+What is 17 - 9?: 1
+That is the incorrect answer. Please Try again.
+What is 17 - 9?: 2
+That is the incorrect answer. Please Try again.
+What is 17 - 9?: 6
+That is incorrect, the correct answer was 8
+
+What is 10 - 7?: 3
+That is correct!
+
+What is 20 - 16?: 4
+That is correct!
+
+What is 16 - 12?: 1
+That is the incorrect answer. Please Try again.
+What is 16 - 12?: 4
+That is correct!
+
+MENU
+(a) Addition
+(s) Subtraction
+(m) Multiplication
+(d) Division
+(e) Exponentiation
+(r) Random Type
+(q) Quit
+Enter Your Choice:m
+Multiplication
+
+What is 7 * 2?: 14
+That is correct!
+
+What is 11 * 9?: 99
+That is correct!
+
+What is 3 * 15?: 4
+That is the incorrect answer. Please Try again.
+What is 3 * 15?: 45
+That is correct!
+
+What is 14 * 13?: 1
+That is the incorrect answer. Please Try again.
+What is 14 * 13?: 2
+That is the incorrect answer. Please Try again.
+What is 14 * 13?: 6
+That is incorrect, the correct answer was 182
+
+MENU
+(a) Addition
+(s) Subtraction
+(m) Multiplication
+(d) Division
+(e) Exponentiation
+(r) Random Type
+(q) Quit
+Enter Your Choice:d
+Division
+
+What is 0 / 1?: 0
+That is correct!
+
+What is 2 / 2?: 1
+That is correct!
+
+What is 10 / 5?: 1
+That is the incorrect answer. Please Try again.
+What is 10 / 5?: 2
+That is correct!
+
+What is 14 / 7?: 1
+That is the incorrect answer. Please Try again.
+What is 14 / 7?: 3
+That is the incorrect answer. Please Try again.
+What is 14 / 7?: 4
+That is incorrect, the correct answer was 2.0
+
+MENU
+(a) Addition
+(s) Subtraction
+(m) Multiplication
+(d) Division
+(e) Exponentiation
+(r) Random Type
+(q) Quit
+Enter Your Choice:e
+Exponentiation
+
+What is 1 ** 11?: 1
+That is correct!
+
+What is 10 ** 1?: 10
+That is correct!
+
+What is 0 ** 12?: 0
+That is correct!
+
+What is 10 ** 2?: 1
+That is the incorrect answer. Please Try again.
+What is 10 ** 2?: 2
+That is the incorrect answer. Please Try again.
+What is 10 ** 2?: 100
+That is correct!
+
+MENU
+(a) Addition
+(s) Subtraction
+(m) Multiplication
+(d) Division
+(e) Exponentiation
+(r) Random Type
+(q) Quit
+Enter Your Choice:r
+Random
+
+What is 19 - 7?: 12
+That is correct!
+
+What is 15 - 12?: 3
+That is correct!
+
+What is 13 - 4?: 9
+That is correct!
+
+What is 20 - 11?: 9
+That is correct!
+
+MENU
+(a) Addition
+(s) Subtraction
+(m) Multiplication
+(d) Division
+(e) Exponentiation
+(r) Random Type
+(q) Quit
+Enter Your Choice:r
+Random
+
+What is 4 + 5?: 9
+That is correct!
+
+What is 17 + 18?: 35
+That is correct!
+
+What is 15 + 0?: 15
+That is correct!
+
+What is 17 + 6?: 23
+That is correct!
+
+MENU
+(a) Addition
+(s) Subtraction
+(m) Multiplication
+(d) Division
+(e) Exponentiation
+(r) Random Type
+(q) Quit
+Enter Your Choice:r
+Random
+
+What is 19 - 11?: 7
+That is the incorrect answer. Please Try again.
+What is 19 - 11?: 8
+That is correct!
+
+What is 7 - 6?: 1
+That is correct!
+
+What is 13 - 1?: 12
+That is correct!
+
+What is 20 - 14?: 6
+That is correct!
+
+MENU
+(a) Addition
+(s) Subtraction
+(m) Multiplication
+(d) Division
+(e) Exponentiation
+(r) Random Type
+(q) Quit
+Enter Your Choice:r
+Random
+
+What is 1 * 15?: 1
+That is the incorrect answer. Please Try again.
+What is 1 * 15?: 15
+That is correct!
+
+What is 6 * 1?: 6
+That is correct!
+
+What is 6 * 3?: 18
+That is correct!
+
+What is 15 * 4?: 60
+That is correct!
+
+MENU
+(a) Addition
+(s) Subtraction
+(m) Multiplication
+(d) Division
+(e) Exponentiation
+(r) Random Type
+(q) Quit
+Enter Your Choice:q
+Thank you for using the CISC106 Basic Math Instructor program. Goodbye.
+RESULTS: Of the 36 questions given to you, you correctly answered 26 on the first try, 5 on the second try, and 1 on the third try. You answered 4 of the 36 questions incorrectly.
+>>> 
+
